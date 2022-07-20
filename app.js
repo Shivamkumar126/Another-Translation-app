435f5e697a25a17867517a960dc6cc6b1ca9208f
s1432g <dmpshivamkumar@gmail.com>
2022-07-20 17:55:50 +0530

    final app

---
+++ app.js
@@ -0,0 +1,32 @@
+var btn = document.querySelector("#btn-translate");
+var inputText = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/pig-latin.json";
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something went wrong with the server")
+}
+
+function clickHandler(){
+
+    
+    var element =  inputText.value;
+
+    fetch(getTranslationURL(element))
+    .then(response => response.json())
+    .then(json => {
+        var trasnlated = json.content.translated;
+        outputDiv.innerTxt = translated;
+    })
+    .catch(errorHandler)
+}
+
+
+btn.addEventListener("click", clickHandler);
+
